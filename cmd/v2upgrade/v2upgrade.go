0c32de22ca5f87a3d40ce10059a0bd7d55c575fe
Katy Moe <katy@katy.moe>
2020-09-29 09:06:07 +0100

    default SDKv2 version v2.0.3

--- cmd/v2upgrade/v2upgrade.go
+++ cmd/v2upgrade/v2upgrade.go
@@ -16,7 +16,7 @@ const (
 	CommandName    = "v2upgrade"
 	oldPackagePath = "github.com/hashicorp/terraform-plugin-sdk"
 	newPackagePath = "github.com/hashicorp/terraform-plugin-sdk/v2"
-	defaultVersion = "master"
+	defaultVersion = "v2.0.3"
 )
 
 var printConfig = printer.Config{
